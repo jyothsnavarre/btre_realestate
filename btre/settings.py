1a5c51b57bbca4e9f901882b438be8e9ae940670
jyothsnavarre <jyothsnavarre14@gmail.com>
2019-06-13 10:35:30 +0530

    first commit

--- btre/settings.py
+++ btre/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER':'postgres',
-        'PASSWORD':'root',
+        'PASSWORD':'jyothsna14',
         'HOST':'localhost'
     }
 }
